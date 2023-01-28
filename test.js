a5821137e95f0d93968a4c9ed9bc079937b8e276
PersonalBM <muirb4@gmail.com>
2023-01-28 16:30:00 +0200

    t

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
+var counter = 1
 var  b = document.getElementById('deselectButton');
-b.addEventListener("click", function() {b.innerHTML = "Hello"} )
+b.addEventListener("click", function() {b.innerHTML = "Clicks: " + counter.toString()} )
 
 export var a = function() {
     print("Hi")
